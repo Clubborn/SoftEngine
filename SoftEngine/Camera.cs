9da427b1af0cc6374c577959bb41780b8476ed93
udalgic23 <160206977+udalgic23@users.noreply.github.com>
2025-01-21 13:35:40 +0300

    Camera Comments

--- SoftEngine/Camera.cs
+++ SoftEngine/Camera.cs
@@ -3,8 +3,10 @@ using System.Numerics;
 namespace SoftEngine;
 
 public class Camera
-{
+{  
+    // Position of Camera Object in the World Space. i.e. Model Matrix of Camera Object
     public Vector3 Position { get; set; }
+    // The Direction where Camera Looks
     public Vector3 Target { get; set; }
 }
 
